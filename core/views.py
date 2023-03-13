0de1c87d1e2afd08a46ad546daf0d33d65dfd0c0
MarcioAlex-x <marcioalex586@gmail.com>
2023-03-13 10:54:10 -0300

    Agenda para projeto em sala de aula

---
+++ core/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+@login_required(login_url='/login/')
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos':evento}
+    return render(request,'agenda.html', dados)
+
+""" def index(request):
+    return redirect('/agenda/') """
+
+def login_users(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha precisa ser corrigido.")
+        return redirect('/')
