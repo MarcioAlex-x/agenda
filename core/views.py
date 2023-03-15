85d14907cebd00e182a5838801ed5f425449e790
MarcioAlex-x <marcioalex586@gmail.com>
2023-03-15 11:04:08 -0300

    Adicionando evento via formulário

--- core/views.py
+++ core/views.py
@@ -33,3 +33,21 @@ def submit_login(request):
         else:
             messages.error(request, "Usuário ou senha precisa ser corrigido.")
         return redirect('/')
+
+@login_required(login_url='/login/')   
+def evento(request):
+    return render (request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        data_evento = request.POST.get('data_evento')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              descricao=descricao,
+                              data_evento=data_evento,
+                              usuario=usuario)
+        
+    return redirect('/')
